Fixes for boundary conditions after product quadrature normalization changes.

Six_1g_spherical_benchmarks/Problem_1.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     xs_mat.CreateSimpleOneGroup(sigma_t=sigt, c=c)
 
     # Boundary source
-    bsrc = [1.0]
+    bsrc = [4.0 * np.pi]
 
     # Angular quadrature
     pquad = GLCProductQuadrature3DXYZ(n_polar=2, n_azimuthal=4)

Six_1g_spherical_benchmarks/Problem_5.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     xs_mat.CreateSimpleOneGroup(sigma_t=1.0, c=0.0)
 
     # Boundary source
-    bsrc = [1.0]
+    bsrc = [4.0 * np.pi]
 
     # Angular quadrature
     pquad = GLCProductQuadrature3DXYZ(n_polar=2, n_azimuthal=4)